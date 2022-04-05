25c7baaddcd7def6bc333ee24be20b8d36ea0ad6
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-04-04 21:22:30 -0700

    commit tree cd150be04335c66f8ad9b3b4daf0e9e8e5449b4c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const url = "http://localhost:3000/users"
+let userData = {
+    name: "Steve",
+    email: "steve@steve.com"
+} 
+let postMethod = {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(userData)
+}
+
+function submitData() {
+    return fetch(url, postMethod)
+        .then(response => response.json())
+        .then(getID)
+        .catch(errors)
+}
+const bodySelector = document.querySelector('body')
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errors(error) {
+    document.body.innerHTML = error.message
+}
